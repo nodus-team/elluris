69e9a807c57339911c5d034712b63c7d0c0a4c5a
ppsousa63 <ppsousa_05@hotmail.com>
2019-11-28 23:32:52 -0300

    Inclusáo de API no projeto

---
+++ src/Nodus.Elluris.WebApi/Controllers/ObrasController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nodus.Elluris.Domain.Models;
+using Nodus.Elluris.WebApi.Repository;
+
+namespace Nodus.Elluris.WebApi.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ObrasController : Controller
+    {
+        private readonly IRepositoryObra _repositoryObra;
+
+        public ObrasController(IRepositoryObra repoObra)
+        {
+            _repositoryObra = repoObra;
+        }
+
+        [HttpGet]
+        public Evento GetEvento()
+        {
+            return _repositoryObra.GetEvento();
+        }
+
+        [HttpGet("{beacon}")]
+        public IActionResult GetObra(string beacon)
+        {
+            var obra = _repositoryObra.Find(beacon);
+
+            return new ObjectResult(obra);
+
+        }
+    }
+}
